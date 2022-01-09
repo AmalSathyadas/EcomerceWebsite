f34a5399ccdeadb0e295b09d43687690515f5520
Amal S S <amalachoos009@gmail.com>
2022-01-09 16:28:19 +0530

    showing information on a html page using context

--- Ewebsite/store/views.py
+++ Ewebsite/store/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'home.html');
+
+def index(request):
+    context={
+        'name': 'Amal',
+        'age': 23,
+        'nationality': 'Indian'
+    }
+    return render(request,'index.html',context);
