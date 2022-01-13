12d9a4ec7b27ef8d2daa682bf2baaa65eb7d2537
Amal S S <amalachoos009@gmail.com>
2022-01-13 17:29:41 +0530

    using models for a static website

--- Ewebsite/store/views.py
+++ Ewebsite/store/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Feature
 # Create your views here.
 def home(request):
     return render(request,'home.html');
 
 def index(request):
-    context={
-        'name': 'Amal',
-        'age': 23,
-        'nationality': 'Indian'
-    }
-    return render(request,'index.html',context);
+    feature1 = Feature()
+    feature1.id=0,
+    feature1.name='Fast'
+    feature1.details='We are So fast'
+
+    feature2 = Feature()
+    feature2.id = 1,
+    feature2.name = 'Reliable'
+    feature2.details = 'We are So fast'
+
+    feature3 = Feature()
+    feature3.id = 2,
+    feature3.name = 'Afordable'
+    feature3.details = 'We are So fast'
+
+    feature4 = Feature()
+    feature4.id = 3,
+    feature4.name = 'easy to use'
+    feature4.details = 'We are So fast'
+
+    return render(request,'index.html',{'feature1':feature1,'feature2':feature2,'feature3':feature3,'feature4':feature4});
 
 def form(request):
 
